860f65fdff398915404db234c869591a78134c4a
kubrakahraman <122174220+kubrakahhraman@users.noreply.github.com>
2026-03-14 17:32:58 +0300

    OpenF1 API'den sürücü verisi çekme scripti eklendi

---
+++ ingestion/fetch_openf1.py
@@ -0,0 +1,15 @@
+import requests
+
+# OpenF1 API'den veri çek
+url = "https://api.openf1.org/v1/drivers?session_key=latest"
+
+response = requests.get(url)
+
+print("Durum kodu:", response.status_code)
+print("Toplam sürücü sayısı:", len(response.json()))
+
+for driver in response.json():
+      print(driver['full_name'], "-", driver["team_name"])
+      
+# print("İlk sürücü:", response.json()[0])
+
