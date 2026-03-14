a0c24212cc9bc72180b871c7b14ed4b77d5747c3
kubrakahraman <122174220+kubrakahhraman@users.noreply.github.com>
2026-03-14 18:22:20 +0300

    Sürücü listesi tablo formatında gösterildi

--- ingestion/fetch_openf1.py
+++ ingestion/fetch_openf1.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # OpenF1 API'den veri çek
 url = "https://api.openf1.org/v1/drivers?session_key=latest"
@@ -6,10 +7,14 @@ url = "https://api.openf1.org/v1/drivers?session_key=latest"
 response = requests.get(url)
 
 print("Durum kodu:", response.status_code)
-print("Toplam sürücü sayısı:", len(response.json()))
+# print("Toplam sürücü sayısı:", len(response.json()))
 
-for driver in response.json():
-      print(driver['full_name'], "-", driver["team_name"])
-      
+# for driver in response.json():
+#       print(driver['full_name'], "-", driver["team_name"])
+
 # print("İlk sürücü:", response.json()[0])
 
+# Veriyi düzenli göster
+print("\n--- SÜRÜCÜ LİSTESİ ---")
+for driver in response.json():
+      print(f"{driver["driver_number"]:3} | {driver["full_name"]:25} | {driver["team_name"]}")
